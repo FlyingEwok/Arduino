838de7c835bb529df7d79b0f786dd7d9c5fe53ad
FlyingEwok <s254525@ocsb.local>
2020-10-08 13:27:27 -0400

    arduino

---
+++ StreetLight-7seg-button.ino
@@ -0,0 +1,231 @@
+// Code: 
+
+int buttonPress = 0;      //inital setup
+
+int button = 2;
+
+int state = 0;
+
+int old = 0;
+
+//7 segement pins
+int a = 12;                     // initialization of the variables that will be the values of the pin
+
+int b = 13;
+
+int c = 8;
+
+int d = 9;
+
+int e = 10;
+
+int f = 11;
+
+int g = 7;
+
+
+
+
+void setup()
+
+{
+
+  pinMode(button,INPUT);
+
+  pinMode(9,OUTPUT);
+
+  pinMode(a, OUTPUT);          // initialization of the arduino OUTPUT pins
+
+  pinMode(b, OUTPUT);
+
+  pinMode(c, OUTPUT);
+
+  pinMode(d, OUTPUT);
+
+  pinMode(e, OUTPUT);
+
+  pinMode(f, OUTPUT);
+
+  pinMode(g, OUTPUT);
+  
+
+}
+
+//Intialize 7 segment display functions
+void nine_dig()
+{
+  digitalWrite(a,LOW);          //to turn a pin on, it has to have a LOW digitalWrite
+  digitalWrite(b,LOW);
+  digitalWrite(c,LOW);
+  digitalWrite(d,HIGH);         //to turn a pin off, it has to have a HIGH digitalWrite
+  digitalWrite(e,HIGH);
+  digitalWrite(f,LOW);
+  digitalWrite(g,LOW);
+}
+
+void eight-dig()
+{
+  digitalWrite(a,LOW);          //to turn a pin on, it has to have a LOW digitalWrite
+  digitalWrite(b,LOW);
+  digitalWrite(c,LOW);
+  digitalWrite(d,LOW);         //to turn a pin off, it has to have a HIGH digitalWrite
+  digitalWrite(e,LOW);
+  digitalWrite(f,LOW);
+  digitalWrite(g,LOW); 
+}
+
+void seven-dig()
+{
+  digitalWrite(a,LOW);          //to turn a pin on, it has to have a LOW digitalWrite
+  digitalWrite(b,LOW);
+  digitalWrite(c,LOW);
+  digitalWrite(d,HIGH);         //to turn a pin off, it has to have a HIGH digitalWrite
+  digitalWrite(e,HIGH);
+  digitalWrite(f,HIGH);
+  digitalWrite(g,HIGH);
+}
+
+void six-dig()
+{
+  digitalWrite(a,LOW);          //to turn a pin on, it has to have a LOW digitalWrite
+  digitalWrite(b,HIGH);
+  digitalWrite(c,LOW);
+  digitalWrite(d,LOW);         //to turn a pin off, it has to have a HIGH digitalWrite
+  digitalWrite(e,LOW);
+  digitalWrite(f,LOW);
+  digitalWrite(g,LOW); 
+}
+
+void five-dig()
+{
+  digitalWrite(a,LOW);          //to turn a pin on, it has to have a LOW digitalWrite
+  digitalWrite(b,HIGH);
+  digitalWrite(c,LOW);
+  digitalWrite(d,LOW);         //to turn a pin off, it has to have a HIGH digitalWrite
+  digitalWrite(e,HIGH);
+  digitalWrite(f,LOW);
+  digitalWrite(g,LOW);
+}
+
+void four-dig()
+{
+  digitalWrite(a,HIGH);          //to turn a pin on, it has to have a LOW digitalWrite
+  digitalWrite(b,LOW);
+  digitalWrite(c,LOW);
+  digitalWrite(d,HIGH);         //to turn a pin off, it has to have a HIGH digitalWrite
+  digitalWrite(e,HIGH);
+  digitalWrite(f,LOW);
+  digitalWrite(g,LOW); 
+}
+
+void three-dig()
+{
+  digitalWrite(a,LOW);          //to turn a pin on, it has to have a LOW digitalWrite
+  digitalWrite(b,LOW);
+  digitalWrite(c,LOW);
+  digitalWrite(d,LOW);         //to turn a pin off, it has to have a HIGH digitalWrite
+  digitalWrite(e,HIGH);
+  digitalWrite(f,HIGH);
+  digitalWrite(g,LOW); 
+}
+
+void two-dig()
+{
+  digitalWrite(a,LOW);          //to turn a pin on, it has to have a LOW digitalWrite
+  digitalWrite(b,LOW);
+  digitalWrite(c,HIGH);
+  digitalWrite(d,LOW);         //to turn a pin off, it has to have a HIGH digitalWrite
+  digitalWrite(e,LOW);
+  digitalWrite(f,HIGH);
+  digitalWrite(g,LOW); 
+}
+
+void one-dig()
+{
+  digitalWrite(a,HIGH);          //to turn a pin on, it has to have a LOW digitalWrite
+  digitalWrite(b,LOW);
+  digitalWrite(c,LOW);
+  digitalWrite(d,HIGH);         //to turn a pin off, it has to have a HIGH digitalWrite
+  digitalWrite(e,HIGH);
+  digitalWrite(f,HIGH);
+  digitalWrite(g,HIGH); 
+}
+
+void zero-dig()
+{
+  digitalWrite(a,LOW);          //to turn a pin on, it has to have a LOW digitalWrite
+  digitalWrite(b,LOW);
+  digitalWrite(c,LOW);
+  digitalWrite(d,LOW);         //to turn a pin off, it has to have a HIGH digitalWrite
+  digitalWrite(e,LOW);
+  digitalWrite(f,LOW);
+  digitalWrite(g,HIGH); 
+}
+
+void loop()
+
+{
+
+  buttonPress = digitalRead(button);
+
+  if(buttonPress == 1){
+
+    delay(50);
+
+    buttonPress = digitalRead(button);
+
+    if (buttonPress == 0 ) {
+
+      state = old + 1;
+ 
+    }}
+
+   else{
+
+    delay(100);
+
+     
+
+
+
+}
+
+
+
+  switch (state) {
+
+    case 1:
+
+      digitalWrite(9,LOW);
+
+      old = state;
+
+      break;
+
+    case 2:
+
+      digitalWrite(9,HIGH);
+
+      old = state;
+
+      break;
+
+    case 3:
+
+      digitalWrite(9,LOW);
+
+      old = state;
+
+      break;
+
+    default:
+
+      digitalWrite(9,HIGH);
+
+      old = 0;
+
+      break;
+
+  }
+
+}
